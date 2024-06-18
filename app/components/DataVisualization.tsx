a65043549b9361893d9740ead6503721342289cf
Charan <charanxmn@gmail.com>
2024-06-18 11:07:45 +0530

    Added the DataVisualization component

---
+++ app/components/DataVisualization.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const DataVisualization: React.FC = () => {
+  return (
+    <div className="bg-white p-4 rounded-md shadow-sm">
+      {/* Charting library integration (e.g., Recharts, Nivo, Chart.js) */}
+      <p className="text-gray-600">
+        Data Visualization Component (Coming Soon)
+      </p>
+    </div>
+  );
+};
+
+export default DataVisualization;
