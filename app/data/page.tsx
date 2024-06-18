8125a3f790fcc25b8e6fb5f934e7799f369be1ff
Charan <charanxmn@gmail.com>
2024-06-18 11:10:56 +0530

    Completed the Data page

---
+++ app/data/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DataVisualization from "@/app/components/DataVisualization";
+
+const Data = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Data</h1>
+      <DataVisualization />
+    </div>
+  );
+};
+
+export default Data;
