193d21556121bb7daaf277434c33fbd9330ff4ab
Charan <charanxmn@gmail.com>
2024-07-15 18:21:20 +0530

    Changed the title

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Your Console",
+  title: "Bio Bro",
   description: "Bioinformatics console application",
 };
 
