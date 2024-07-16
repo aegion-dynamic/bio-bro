eb99315999d3db66873e45a288572f64c3d221e0
Charan <charanxmn@gmail.com>
2024-07-16 17:27:16 +0530

    Update Workflows page
    Modified the Workflows page to use the data from a different js file.

--- app/workflows/page.jsx
+++ app/workflows/page.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import WorkflowCard from "@/app/components/WorkflowCard";
+import { workflows } from "app/utils/workflowsData";
+import PageContainer from "app/components/PageContainer";
+import GridContainer from "app/components/GridContainer";
 
 const Workflows = () => {
-  const workflows = [
-    { name: "RNA Sequencing Analysis", status: "running" },
-    { name: "Variant Calling", status: "completed" },
-    // ... more workflows
-  ];
-
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Workflows</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <PageContainer name="Workflows">
+      <GridContainer>
         {workflows.map((workflow) => (
           <WorkflowCard key={workflow.name} {...workflow} />
         ))}
-      </div>
-    </div>
+      </GridContainer>
+    </PageContainer>
   );
 };
 
