ac658d48fc9ec3c1f7afbf505b4f33768923c649
Charan <charanxmn@gmail.com>
2024-07-16 16:47:21 +0530

    Update Dashboard page
    Modified to use the new reusable wrapper components

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,22 +4,27 @@ import React from "react";
 import Card from "@/app/components/Card";
 import WorkflowCard from "@/app/components/WorkflowCard";
 import Button from "@/app/components/Button";
+import PageContainer from "app/components/PageContainer";
+import GridContainer from "app/components/GridContainer";
+import { workflows } from "app/utils/workflowsData";
 
 const Dashboard = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-
+    <PageContainer name="Dashboard">
       <Button onClick={() => console.log("Create new workflow")}>
         Create New Workflow
       </Button>
 
       <Card title="Recent Workflows">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <WorkflowCard name="RNA Sequencing Analysis" status="running" />
-        </div>
+        <GridContainer>
+          {workflows.map((workflow, index) => {
+            if (index + 1 == workflows.length) return;
+            console.log(workflows.length);
+            return <WorkflowCard key={workflow.name} {...workflow} />;
+          })}
+        </GridContainer>
       </Card>
-    </div>
+    </PageContainer>
   );
 };
 
