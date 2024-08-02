f4e9e5472111a5491938dbda6a007b81dcae777b
Charan <charanxmn@gmail.com>
2024-08-02 11:55:52 +0530

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,21 +1,18 @@
 "use client";
-
+=
 import React from "react";
 import Card from "@/app/components/Card";
 import WorkflowCard from "@/app/components/WorkflowCard";
 import Button from "@/app/components/Button";
 import PageContainer from "app/components/PageContainer";
+import PageSection from "app/components/PageSection";
 import GridContainer from "app/components/GridContainer";
 import { workflows } from "app/utils/workflowsData";
 
 const Dashboard = () => {
   return (
     <PageContainer name="Dashboard">
-      <Button onClick={() => console.log("Create new workflow")}>
-        Create New Workflow
-      </Button>
-
-      <Card title="Recent Workflows">
+      <PageSection name="Recent Workflows">
         <GridContainer>
           {workflows.map((workflow, index) => {
             if (index + 1 == workflows.length) return;
@@ -23,7 +20,10 @@ const Dashboard = () => {
             return <WorkflowCard key={workflow.name} {...workflow} />;
           })}
         </GridContainer>
-      </Card>
+        <Button onClick={() => console.log("Create new workflow")}>
+          Create New Workflow
+        </Button>
+      </PageSection>
     </PageContainer>
   );
 };
