0a68c294bbc903013eb38c4013fdb4347c12de93
Charan <charanxmn@gmail.com>
2024-08-02 12:19:22 +0530

    chore: Update Dashboard page to use reusable wrapper components and add Create New Workflow and View All Workflows buttons

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-=
 import React from "react";
 import Card from "@/app/components/Card";
 import WorkflowCard from "@/app/components/WorkflowCard";
@@ -8,6 +7,7 @@ import PageContainer from "app/components/PageContainer";
 import PageSection from "app/components/PageSection";
 import GridContainer from "app/components/GridContainer";
 import { workflows } from "app/utils/workflowsData";
+import Link from "node_modules/next/link";
 
 const Dashboard = () => {
   return (
@@ -20,9 +20,18 @@ const Dashboard = () => {
             return <WorkflowCard key={workflow.name} {...workflow} />;
           })}
         </GridContainer>
-        <Button onClick={() => console.log("Create new workflow")}>
-          Create New Workflow
-        </Button>
+        <GridContainer>
+          <Button onClick={() => console.log("Create new workflow")}>
+            Create New Workflow
+          </Button>
+          <Link
+            href="/workflows"
+            className="px-4 py-2 rounded-md text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-150 ease-in-ou bg-gray-200 hover:bg-gray-300 text-gray-700"
+            style={{ "text-align": "center" }}
+          >
+            View All Workflows
+          </Link>
+        </GridContainer>
       </PageSection>
     </PageContainer>
   );
