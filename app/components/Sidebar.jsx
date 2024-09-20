e35cf87495c0a30531e1a3cff80f3c46e5d2505d
Charan <charanxmn@gmail.com>
2024-09-20 10:55:11 +0530

    Update Sidebar.jsx

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { links } from "app/utils/sidebarLinks";
+import { links } from "/app/utils/sidebarLinks";
 
 const Sidebar = () => {
   const pathname = usePathname();
