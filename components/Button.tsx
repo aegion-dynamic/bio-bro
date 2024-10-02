4d108106df6d1c62b5ceecb264181cbed57c299c
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-02 07:39:54 +0530

    Modified the Button for a better understanding of storybook example

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,23 @@
-const Button = () => {
-  return <button className="bg-slate-600 text-white px-1 py-2">Dummy</button>;
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  variant?: "primary" | "secondary";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = "primary",
+}) => {
+  const buttonClasses = `bg-${variant}-500 hover:bg-${variant}-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`;
+
+  return (
+    <button className={buttonClasses} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
+
 export default Button;
